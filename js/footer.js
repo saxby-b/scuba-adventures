7ef58815c0576a32093c2d58c566bd7ef3777e1b
C. Saxby Bain <csaxbybain@yahoo.com>
2025-04-17 14:24:08 -0400

    Initial commit

---
+++ js/footer.js
@@ -0,0 +1,25 @@
+const Footer = () => (
+  <div class="content-wrapper">
+    <div class="footer-content">
+    <div class="content">
+      <h3> Contact Info:</h3>
+      <address>
+        <a href="mailto:Scuba@Adventures.com">Scuba@Adventures.com</a>
+        <p>
+          12 Coral Cove,
+          <br />
+          Town, Place 55555
+        </p>
+      </address>
+      </div>
+      <div class="social">
+        <h3>Find Us:</h3>
+        <ul>
+            <li><a href="https://www.facebook.com/" target="_blank"></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank"></a></li>
+            <li><a href="https://x.com/" target="_blank"></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
