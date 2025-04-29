49d52a12898c7c88282b6a58fda3bef803047c63
C. Saxby Bain <csaxbybain@yahoo.com>
2025-04-29 15:19:39 -0400

    Added event listener to email input

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const year = document.querySelector("span");
+const email = document.querySelector(".email");
+const submit = document.querySelector(".submit");
 
+email.addEventListener("click", function (e) {
+    e.target.value = "";
+
+})
+
+/*submit.addEventListener("click", function () {
+    email.defaultRefresh();
+} ) */
 const d = new Date();
 const y = d.getFullYear();
 year.innerText = y;
