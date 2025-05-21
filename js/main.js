022ff64c5f2982aac4bfd56acaa26898f5e17b59
C. Saxby Bain <csaxbybain@yahoo.com>
2025-05-21 15:57:35 -0400

    Added new variables to try and change adventure layout later

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ const year = document.querySelector("span");
 const email = document.querySelector(".email");
 const submit = document.querySelector(".submit");
 
+const section1 = document.querySelector(".section1");
+const section2 = document.querySelector(".section2");
+const section3 = document.querySelector(".section3");
+const section4 = document.querySelector("section4");
+
+const sectionArray = [section1, section2, section3, section4];
+
+
+
 email.addEventListener("click", function (e) {
     e.target.value = "";
 
