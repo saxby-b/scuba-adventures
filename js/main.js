513f13cb9847e33b20a939a980c248d059b148d5
C. Saxby Bain <csaxbybain@yahoo.com>
2025-05-23 17:14:30 -0400

    Fixed code issue for section4

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const adventure = document.querySelector("#adventure");
 const section1 = document.querySelector(".section1");
 const section2 = document.querySelector(".section2");
 const section3 = document.querySelector(".section3");
-const section4 = document.querySelector("section4");
+const section4 = document.querySelector(".section4");
 const next = document.querySelector(".next");
 
 const sectionArray = [section1, section2, section3, section4];
